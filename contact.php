fe02916d6dfe482cd21dbfd9b71017c8dcbf1bd5
naman <namansoni329@gmail.com>
2016-02-04 23:41:32 +0530

    Second Commit

--- contact.php
+++ contact.php
@@ -40,8 +40,169 @@
   <div class="contacts">
       <h3>Naman Kumar Soni</h3>
          Work in progress.<br><br>
+     
+
+
+  </div>
+    
+</div>
+
+<div id="footer">
+Copyright © IT Department
+</div>
+
+</body>
+</html>
+<?php
+    include("include/conn.php");
+?>
+
+<html>
+<head>
+
+
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body  style="background-image:url(pictures/bgimg.jpg); background-repeate:no-repeate;">
+
+
+
+    <div class="image">
+        <img src="pictures/logo.png" style="width:150px;height:150px; float:right;">
+
+    </div>
+      <h1 style="float:left; margin:50px; font-size:50px; color:rgb(0, 63, 179);">NIT DURGAPUR</h1>
+    <br><br><br><br><br><br><br><br>
+
+<div id="header">
+    
+    <ul>
+         <li class="menu"><a href="index.php"  style="text-decoration:none; color:rgb(0, 63, 179); "><b>HOME<b></a></li>
+         <li class="menu"><a href="about.php" style="text-decoration:none;color:rgb(0, 63, 179);">ABOUT</a></li>
+          <li class="menu"><a href="contact.php" style="text-decoration:none; color:rgb(0, 63, 179);">CONTACTS</a></li>
+    </ul>
+</div>
+
+<div id="nav">
+<a href="student.php" class="myButton">student</a><br>
+<a href="admin.php" class="myButton">admin</a><br>
+<a href="#" class="myButton">about</a><br>
+</div>
+
+<div id="section">
+  <div class="contacts">
       <h3>Naman Kumar Soni</h3>
          Work in progress.<br><br>
+     
+
+
+  </div>
+    
+</div>
+
+<div id="footer">
+Copyright © IT Department
+</div>
+
+</body>
+</html>
+<?php
+    include("include/conn.php");
+?>
+
+<html>
+<head>
+
+
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body  style="background-image:url(pictures/bgimg.jpg); background-repeate:no-repeate;">
+
+
+
+    <div class="image">
+        <img src="pictures/logo.png" style="width:150px;height:150px; float:right;">
+
+    </div>
+      <h1 style="float:left; margin:50px; font-size:50px; color:rgb(0, 63, 179);">NIT DURGAPUR</h1>
+    <br><br><br><br><br><br><br><br>
+
+<div id="header">
+    
+    <ul>
+         <li class="menu"><a href="index.php"  style="text-decoration:none; color:rgb(0, 63, 179); "><b>HOME<b></a></li>
+         <li class="menu"><a href="about.php" style="text-decoration:none;color:rgb(0, 63, 179);">ABOUT</a></li>
+          <li class="menu"><a href="contact.php" style="text-decoration:none; color:rgb(0, 63, 179);">CONTACTS</a></li>
+    </ul>
+</div>
+
+<div id="nav">
+<a href="student.php" class="myButton">student</a><br>
+<a href="admin.php" class="myButton">admin</a><br>
+<a href="#" class="myButton">about</a><br>
+</div>
+
+<div id="section">
+  <div class="contacts">
+      <h3>Naman Kumar Soni</h3>
+         Work in progress.<br><br>
+     
+
+
+  </div>
+    
+</div>
+
+<div id="footer">
+Copyright © IT Department
+</div>
+
+</body>
+</html>
+<?php
+    include("include/conn.php");
+?>
+
+<html>
+<head>
+
+
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body  style="background-image:url(pictures/bgimg.jpg); background-repeate:no-repeate;">
+
+
+
+    <div class="image">
+        <img src="pictures/logo.png" style="width:150px;height:150px; float:right;">
+
+    </div>
+      <h1 style="float:left; margin:50px; font-size:50px; color:rgb(0, 63, 179);">NIT DURGAPUR</h1>
+    <br><br><br><br><br><br><br><br>
+
+<div id="header">
+    
+    <ul>
+         <li class="menu"><a href="index.php"  style="text-decoration:none; color:rgb(0, 63, 179); "><b>HOME<b></a></li>
+         <li class="menu"><a href="about.php" style="text-decoration:none;color:rgb(0, 63, 179);">ABOUT</a></li>
+          <li class="menu"><a href="contact.php" style="text-decoration:none; color:rgb(0, 63, 179);">CONTACTS</a></li>
+    </ul>
+</div>
+
+<div id="nav">
+<a href="student.php" class="myButton">student</a><br>
+<a href="admin.php" class="myButton">admin</a><br>
+<a href="#" class="myButton">about</a><br>
+</div>
+
+<div id="section">
+  <div class="contacts">
       <h3>Naman Kumar Soni</h3>
          Work in progress.<br><br>
      
